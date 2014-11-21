87c438e6e6d075061851af05bda74a544ebf0013
Chung Jason <shiningjason1989@gmail.com>
2014-11-21 13:02:52 +0800

    第三步：加入 JPA 的 Annotation

--- src/main/java/net/lustertech/training/data/domains/Player.java
+++ src/main/java/net/lustertech/training/data/domains/Player.java
@@ -1,7 +1,15 @@
 package net.lustertech.training.data.domains;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Player {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String username;
