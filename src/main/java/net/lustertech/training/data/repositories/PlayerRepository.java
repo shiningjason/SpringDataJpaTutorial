6304dab04f58f6f85854dd5936786542f9e61c48
Chung Jason <shiningjason1989@gmail.com>
2014-11-21 17:43:48 +0800

    第二步：加入 findByNickname

--- src/main/java/net/lustertech/training/data/repositories/PlayerRepository.java
+++ src/main/java/net/lustertech/training/data/repositories/PlayerRepository.java
@@ -4,4 +4,6 @@ import net.lustertech.training.data.domains.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
+
+    public Player findByNickname(String nickname);
 }
