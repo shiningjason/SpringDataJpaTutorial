90c11dff8057a58245f1a940895cec5577f2cd88
Chung Jason <shiningjason1989@gmail.com>
2014-11-21 13:01:35 +0800

    第二步：加入所有 Player 的 Field

--- src/main/java/net/lustertech/training/data/domains/Player.java
+++ src/main/java/net/lustertech/training/data/domains/Player.java
@@ -1,4 +1,22 @@
 package net.lustertech.training.data.domains;
 
 public class Player {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
 }
