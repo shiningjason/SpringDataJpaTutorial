ac8932071eeeb9f444a29b6f677a94447d47a30e
Chung Jason <shiningjason1989@gmail.com>
2014-11-21 14:48:56 +0800

    第四步：檢測取得的玩家數量是否正確

--- src/test/java/net/lustertech/training/data/GameTests.java
+++ src/test/java/net/lustertech/training/data/GameTests.java
@@ -2,12 +2,15 @@ package net.lustertech.training.data;
 
 import net.lustertech.training.data.domains.Player;
 import net.lustertech.training.data.repositories.PlayerRepository;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
@@ -36,5 +39,7 @@ public class GameTests {
         for (Player player : players) System.out.println(player);
 
         System.out.println("＝＝＝＝＝＝＝＝＝＝");
+
+        assertThat(players, Matchers.<Player>iterableWithSize(3));
     }
 }
